2c00bd00509c9b26160f28fd1aae76dd95a0d482
Kyunghoon Kim <koorukuroo@gmail.com>
2025-02-18 22:24:19 +0900

    Initialize for coredotcloud

--- setup.py
+++ setup.py
@@ -1,49 +1,25 @@
-#!/usr/bin/env python
-
-"""The setup script."""
-
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = ['Click>=7.0', ]
-
-test_requirements = ['pytest>=3', ]
-
 setup(
-    author="CoreDotToday",
-    author_email='engine@core.today',
-    python_requires='>=3.6',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    name="coredotcloud",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["psutil", "requests"],
     entry_points={
-        'console_scripts': [
-            'coredotcloud=coredotcloud.cli:main',
-        ],
+        "console_scripts": [
+            "coredotcloud=coredotcloud.main:main"
+        ]
     },
-    install_requires=requirements,
-    license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='coredotcloud',
-    name='coredotcloud',
-    packages=find_packages(include=['coredotcloud', 'coredotcloud.*']),
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/coredottoday/coredotcloud',
-    version='0.1.0',
-    zip_safe=False,
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple system monitoring tool for CoreDotCloud",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/coredotcloud",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )
